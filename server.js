d4f142f57692e901155fd2dcd41cca7ed4f87ab9
Oluwatimilehin <oluwatimilehin.id@gmail.com>
2021-01-04 14:02:32 +0100

    modified server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  // res.sendFile(__dirname + '/views/index.html');
+  res.redirect("/api/timestamp");
 });
 
 
@@ -29,4 +30,29 @@ app.get("/api/hello", function (req, res) {
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+let res = {};
+
+app.get('/api/timestamp/:date', (request, response) => {
+  let date = request.params.date;
+  if(date.includes('-')) {
+    res.unix = new Date(date).getTime();
+    res.utc = new Date(date).toUTCString();
+  } else {
+    date = parseInt(date);
+    res.unix = new Date(date).getTime();
+    res.utc = new Date(date).toUTCString();
+  }
+  if((!res.unix) || (!res.utc) ) {
+    response.json({ error: "Invalid Date" });
+  }
+  response.json(res);
+})
+
+app.get("/api/timestamp", (request, response) => {
+  res.unix = new Date().getTime();
+  res.utc = new Date().toUTCString();
+
+  response.json(res);
 });
